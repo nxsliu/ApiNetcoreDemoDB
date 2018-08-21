fb2d7b2de939456914b2019908286c4d1e6ac4f1
Nathan Liu <nxsliu@gmail.com>
2018-08-21 17:47:19 +0100

    Added Column

--- ApiDemoDb/dbo/Tables/Products.sql
+++ ApiDemoDb/dbo/Tables/Products.sql
@@ -2,6 +2,7 @@
     [Id] UNIQUEIDENTIFIER NOT NULL,
     [Name] VARCHAR(50) NOT NULL, 
     [Balance] DECIMAL(19, 4) NOT NULL, 
+    [Active] BIT NOT NULL, 
     PRIMARY KEY CLUSTERED ([Id] ASC)
 );
 
